c012c0c519efc7bf5871ef578129dbde7bfff568
Ablasse Kingcaid-Ouedraogo <ablassecode@outlook.com>
2025-11-01 02:37:40 -0500

    misc changes

--- linux/debian/first-time-setup.sh
+++ linux/debian/first-time-setup.sh
@@ -1,11 +1,13 @@
-sudo apt-get update && sudo apt upgrade -y
+sudo apt-get update
+sudo apt upgrade -y
+sudo apt install git gh curl make
 git clone https://github.com/asdf-vm/asdf.git --branch v0.18.0 && cd asdf && make && cp ./bin/asdf /usr/local/bin/
 echo "export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"" >> ~/.bash_profile
 echo "source ~/.bash_profile" >> ~/.bashrc
-source ~/.bashrc
+source "$HOME/.bashrc"
 for url in  "https://github.com/asdf-vm/asdf-nodejs.git" "https://github.com/asdf-vm/asdf-ruby.git" "https://github.com/asdf-community/asdf-python" "https://github.com/asdf-community/asdf-rust.git"; do
-  plugin = "${url##*asdf-}"
+  plugin=${url##*asdf-}
   asdf plugin add $plugin $url
   asdf install $plugin latest
 done
-sudo apt-get install -y dirmngr gpg curl gawk
+sudo apt-get install -y dirmngr gpg gawk
